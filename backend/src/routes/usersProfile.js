897e605b1686bd2963a24c663d1275be0739dd60
rdendonc <rachel.dendoncker@gmail.com>
2025-08-04 14:59:59 +0200

    add history db and history profile

--- backend/src/routes/usersProfile.js
+++ backend/src/routes/usersProfile.js
@@ -1,15 +1,16 @@
-import { openDb } from '../utils/db.js';
+import { openDb, openDbHistory } from '../utils/db.js';
 
 export default async function userProfileRoutes(fastify)
 {
 	fastify.get('/user/:name', async (req, reply) => {
-		const db = await openDb();
+		const historyDb = await openDbHistory();
 		const userName = req.params.name;
 
-		const user = await db.get('SELECT * FROM users WHERE name = ?', userName);
-		if (user)
-			reply.send(user);
-		else
-			reply.code(404).send({ error: "Utilisateur inconnu"});
+		const history = await historyDb.all('SELECT * FROM history WHERE player_1 = ? OR player_2 = ? ORDER BY created_at DESC', userName, userName
+	);
+	if (history)
+		reply.send(history);
+	else
+		reply.code(404).send({ success: false, message: "Problème pendant l'afichage du profil" });
 	});
 }
--- frontend/src/pages/usersProfile.ts
+++ frontend/src/pages/usersProfile.ts
@@ -6,16 +6,40 @@ export async function renderUserProfile(ctx: any) {
 	console.log("renderUserProfile called");
 	try {
 		const userName = ctx.params.name;
-		const res = await fetch(`api/user/${encodeURIComponent(userName)}`, { method: "GET" });
+		const historyRes = await fetch(`api/user/${encodeURIComponent(userName)}`, { method: "GET" });
 
-		if (!res.ok) {
+		if (!historyRes.ok) {
 			document.getElementById("app")!.innerHTML = "<p>Cet utilisateur n'existe pas</p>";
 			return;
 		}
-		const user = await res.json();
+		const history = await historyRes.json();
 
 		const html = `
-		<h1 style="text-align: center;">Profile de ${user.name}</h1>
+		<h1 style="text-align: center;">Profile de ${userName}</h1>
+		<table border="1" style="width: 100%; text-align: center;">
+			<thead>
+				<tr>
+					<th>Type</th>
+					<th>Joueur 1</th>
+					<th>Joueur 2</th>
+					<th>Score</th>
+					<th>Vainqueur</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				${history.map((entry: any) => `
+					<tr>
+						<td>${entry.type}</td>
+						<td>${entry.player_1}</td>
+						<td>${entry.player_2}</td>
+						<td>${entry.scores}</td>
+						<td>${entry.winner}</td>
+						<td>${new Date(entry.created_at).toLocaleString()}</td>
+					</tr>
+				`).join("")}
+			</tbody>
+			</table>
 			${backButton()}
 		`;
 
