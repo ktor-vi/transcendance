4caad3f7f6bf30bcff0e16f03f38a7bc3a9463e8
KevinDegryse <kevinarnauddegryse@gmail.com>
2025-08-26 12:40:20 +0200

    clean

--- backend/src/routes/userChat.js
+++ backend/src/routes/userChat.js
@@ -6,28 +6,30 @@ import { openDb, openChatDB } from '../utils/db.js';
  */
 export default async function privateChat(fastify)
 {
-	fastify.get('/chat', async (req, reply) => {
+	fastify.get('/chat/:name', async (req, reply) => {
 		const userDB = await openDb();
 		const chatDB = await openChatDB();
-		const user1 = req.user.id;
-		const user2 = req.params.name;
+		let userFrom = req.user.id;
+		let userTo = req.params.name;
 
 		let conversationID = await chatDB.get(`
 			SELECT * FROM conversations
 			WHERE (user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?)`,
-			user1, user2, user2, user1);
+			userFrom, userTo, userTo, userFrom);
 		if (!conversationID){
-			const result = await db.run(`
+			let result = await chatDB.run(`
       			INSERT INTO conversations (user1_id, user2_id)
       			VALUES (?, ?)`,
-				user1, user2);
+				userFrom, userTo);
     		conversationID = { id: result.lastID };
 		}
-		let conversation = await chatDB.all('SELECT * FROM messages \
-			WHERE conversation_id = ? ORDER BY created_at DESC',
+		let conversation = await chatDB.all(`
+			SELECT * FROM messages
+			WHERE conversation_id = ? ORDER BY created_at DESC`,
 			conversationID);
 	if (!conversation)
 		reply.code(404).send({ success: false, message: "Problème pendant l'affichage du chat" });
-	reply.send(conversation);
+	else
+		reply.send(conversation);
 	});
 }
