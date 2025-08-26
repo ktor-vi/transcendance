cb87adef44b1ca6dcd2f423e173e6fc828e93bd3
rdendonc <rachel.dendoncker@gmail.com>
2025-08-26 11:52:58 +0200

    add requests page with list and icons

--- backend/src/routes/friends.js
+++ backend/src/routes/friends.js
@@ -6,6 +6,7 @@ import { fileTypeFromBuffer } from "file-type";
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function friendsRoutes(fastify) {
+	// route pour récupérer le nombre de requêtes qu'on a en attente
 	fastify.get('/requests', async (req, reply) => {
 	
 		const userSession = req.session.get('user');
@@ -22,7 +23,7 @@ export default async function friendsRoutes(fastify) {
 		
 		reply.send(requests.total);
 	});
-
+	// route pour afficher la liste de nos amis
 	fastify.get('/friends', async (req, reply) => {
 
 		const userSession = req.session.get('user');
@@ -34,21 +35,20 @@ export default async function friendsRoutes(fastify) {
 		const userName = userSession.name;
 		const userRow = await db.get('SELECT * FROM users WHERE name = ?', userName);
 		const userId = userRow.id;
-
-		const friendsTable = await db.all('SELECT * FROM friends WHERE user1_id = ? OR user2_id = ?', userId, userId);
-		console.log("Table d'amis :");
-		console.log(friendsTable);
-
+		
 		const friendsList = await db.all(`
 			SELECT
-			CASE WHEN friends.user1_id = ? THEN u2.name
-      		ELSE u1.name END AS friend_name,
-    		friends.friends_since FROM friends
-  			JOIN users u1 ON friends.user1_id = u1.id
-  			JOIN users u2 ON friends.user2_id = u2.id
-  			WHERE friends.user1_id = ? OR friends.user2_id = ?
+			CASE WHEN friends.user1_id = ?
+			THEN u2.name
+			ELSE u1.name END AS friend_name,
+			friends.friends_since FROM friends
+			JOIN users u1 ON friends.user1_id = u1.id
+			JOIN users u2 ON friends.user2_id = u2.id
+			WHERE friends.user1_id = ? OR friends.user2_id = ?
 			`, userId, userId, userId);
-
+			
+		console.log("List d'amis :");
+		console.log(friendsList);
 
 		const totalFriends = await db.get('SELECT COUNT (*) AS total FROM friends WHERE user1_id = ? OR user2_id = ?', userId, userId);
 		console.log("Nombre d'amis :");
@@ -57,5 +57,36 @@ export default async function friendsRoutes(fastify) {
 			total: totalFriends.total,
 			friends: friendsList
 		});
+	});
+	// route qui gère la page des requêtes
+	fastify.get('/friends/requests', async (req, reply) => {
+		const userSession = req.session.get('user');
+		const db = await openDb();
+
+		if (!userSession) {
+			return reply.code(401).send({ error: 'Non connecté' });
+		}
+
+		const userName = userSession.name;
+		const userRow = await db.get('SELECT * FROM users WHERE name = ?', userName);
+		const receiverId = userRow.id;
+
+		const requestsTable = await db.all('SELECT sender_id, request_date FROM requests WHERE receiver_id = ? AND status = ?', receiverId, "waiting");
+		console.log("Table des requêtes :");
+		console.log(requestsTable);
+
+		const sendersList = await db.all(`
+			SELECT
+				u.name AS sender_name,
+				requests.request_date
+			FROM requests
+			JOIN users u
+				ON requests.sender_id = u.id
+			WHERE requests.receiver_id = ?
+				AND requests.status= 'waiting'
+			`, receiverId);
+		console.log("Noms des sender :");
+		console.log(sendersList);
+		reply.send(sendersList );
 	});
 }
--- frontend/src/pages/friendsRequests.ts
+++ frontend/src/pages/friendsRequests.ts
@@ -6,10 +6,54 @@ export async function renderFriendsRequests() {
 	
 		const html = `
 		<h1 style="text-align: center;">Demandes d'amis :</h1>
+		<ul id="requestsList" style="background: none;"></ul>
 		${backButton()}
 		`;
 
 		document.getElementById("app")!.innerHTML = html;
 		setupBackButton();
+
+		const resRequests = await fetch("/api/friends/requests", { method: "GET" });
+
+		const listRequests = document.getElementById("requestsList");
+		const requestsData = await resRequests.json();
+
+		console.log("data = ");
+		console.log(requestsData);
+
+		if (listRequests) {
+			listRequests.innerHTML = "";
+			for (const line of requestsData) {
+				const li = document.createElement("li");
+				li.className = "flex items-center justify-between p-2 border-b";
+
+				const span1 = document.createElement("span");
+				span1.textContent = `${line.sender_name}`;
+
+				const span2 = document.createElement("span");
+				span2.textContent = `${line.request_date}`;
+
+				const divButtons = document.createElement("div");
+
+				const accept = document.createElement("button");
+				accept.className = "icons-btn";
+				accept.innerHTML = `<img src="/images/ok-svgrepo-com.svg" alt="Accepter" class="w-10">`;
+
+				const decline = document.createElement("button");
+				decline.className = "icons-btn";
+				decline.innerHTML = `<img src="/images/cancel-svgrepo-com.svg" alt="Refuser" class="w-10">`;
+
+
+				divButtons.appendChild(accept);
+				divButtons.appendChild(decline);
+
+				li.appendChild(span1);
+				li.appendChild(span2);
+				li.appendChild(divButtons);
+
+				listRequests.appendChild(li);
+			}
+		}
+
 
 }
