ef78646f96ea2a8bd0a42cd89b1dabd1aecc3140
rdendonc <rachel.dendoncker@gmail.com>
2025-09-10 16:25:48 +0200

    start front home

--- frontend/src/pages/home.ts
+++ frontend/src/pages/home.ts
@@ -1,22 +1,16 @@
 export function renderHome()
 {
 	return `
-	<section style="text-align:center; margin-top:2vh;">
-		<img src="/images/HelloKitty.png" alt="default" style="display: block; margin: 0 auto; width: 15%;" />
-		<h1 style="font-size: 4vh; margin-top: 2vh;">🍓 TRANSCENDANCE 🍓</h1>
-		<h3>Made with love by vphilipp, rdendonc, jepatern (🪦), iait-ouf and kdegryse</h3>
-		<div style="text-align:center; margin-top:2vh;">
-			<button id="google-sign-in">Se connecter avec Google</button>
-			<a href="login" data-nav>Se connecter</a>
-			<a href="register" data-nav>S'inscrire</a>
-		</div>
-		<a href="forgotPassword" style="background: none; display: block; margin-top: 2vh;" data-nav>Mot de passe oublié</a>
+	<section class="flex flex-col items-center text-center mt-[2vh] space-y-4">
+	<img src="/images/hellokittycomputer.png" class="hellokitty-computer">
+		<h1 class="text-6xl">TRANSCENDENCE</h1>
+		<h3>made with love by vphilipp, rdendonc, iait-ouf and kdegryse</h3>
+			<div>
+				<button id="google-sign-in">Se connecter avec Google</button>
+				<a href="login" data-nav>Se connecter</a>
+				<a href="register" data-nav>S'inscrire</a>
+			</div>
+		<a href="forgotPassword" data-nav>Mot de passe oublié</a>
 	</section>
-	<div class="whitespace-nowrap">
-		<img src="/images/cloud1.svg" class="inline-block w-44 h-auto" />
-		<img src="/images/cloud2.svg" class="inline-block w-44 h-auto" />
-		<img src="/images/cloud4.svg" class="inline-block w-44 h-auto" />
-	</div>
-
 	`;
 }
