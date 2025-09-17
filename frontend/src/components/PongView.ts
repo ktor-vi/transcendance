49ea709cfe5ec9b135866f51a497055fe9b31079
KevinDegryse <kevinarnauddegryse@gmail.com>
2025-09-17 12:30:37 +0200

    cute images

--- frontend/src/components/PongView.ts
+++ frontend/src/components/PongView.ts
@@ -18,20 +18,17 @@ import {
   Control
 } from "@babylonjs/gui";
 
+// const FIELD_IMAGE = "https://doc.babylonjs.com/img/resources/textures_thumbs/grass.dds.jpg";
+// const PADDLE_IMAGE = "https://www.babylonjs-playground.com/textures/amiga.jpg";
+// const BALL_IMAGE = "https://us1.discourse-cdn.com/flex024/uploads/babylonjs/original/3X/7/b/7b835f51e968cd202d62ad1277dac879e19ffd9b.png";
+// const WALL_IMAGE = "https://www.babylonjs-playground.com/textures/crate.png";
 const GRAPHIC_FOLDER = "../../public/images/";
 const SKYBOX_IMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzRT6G0cuCqmybwjD-8zWjjUIAQBvi6LMFHkTCZL5hzSEtDgYqIdfWRLWtnyG4SBGBptk&usqp=CAU";
-const FIELD_IMAGE = "https://doc.babylonjs.com/img/resources/textures_thumbs/grass.dds.jpg";
-const PADDLE_IMAGE = "https://www.babylonjs-playground.com/textures/amiga.jpg";
-// const BALL_IMAGE = GRAPHIC_FOLDER + "HelloKitty.png";
-const BALL_IMAGE = "https://us1.discourse-cdn.com/flex024/uploads/babylonjs/original/3X/7/b/7b835f51e968cd202d62ad1277dac879e19ffd9b.png";
-const WALL_IMAGE = "https://www.babylonjs-playground.com/textures/crate.png";
-const TILE_SIZE = 0.5;
+const FIELD_IMAGE = GRAPHIC_FOLDER + "floweryground.jpg";
+const PADDLE_IMAGE = GRAPHIC_FOLDER + "paddle.jpeg";
+const BALL_IMAGE = GRAPHIC_FOLDER + "fluffyball.webp";
+const WALL_IMAGE = GRAPHIC_FOLDER + "shinycat.jpg";
 const SHINY_IMAGE = "https://playground.babylonjs.com/textures/flare.png";
-
-//care !!!!
-const FIELD_WIDTH = 13.5;
-const FIELD_DEPTH = 7.5;
-const WALL_HEIGHT = 1.5;
 
 export class PongView {
   engine: Engine;
@@ -97,13 +94,13 @@ export class PongView {
     let toRender = this.scene.getActiveMeshCandidates().data;
     for (let i = 0; i < toRender.length; i++){
       if (toRender[i].name === "wall")
-        applyTexture(toRender[i], WALL_IMAGE, WALL_HEIGHT / TILE_SIZE, FIELD_DEPTH / TILE_SIZE, this.scene);
+        applyTexture(toRender[i], WALL_IMAGE, 1, 9, this.scene);
       else if (toRender[i].name === "paddle")
         applyTexture(toRender[i], PADDLE_IMAGE, 1, 1, this.scene);
       else if (toRender[i].name === "ball")
         applyTexture(toRender[i], BALL_IMAGE, 1, 1, this.scene);
       else if (toRender[i].name === "ground")
-        applyTexture(toRender[i], FIELD_IMAGE, FIELD_WIDTH / TILE_SIZE, FIELD_DEPTH / TILE_SIZE, this.scene);
+        applyTexture(toRender[i], FIELD_IMAGE, 15, 10, this.scene);
     }
   }
 
